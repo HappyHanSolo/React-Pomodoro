a72eff070e4ce2e87ffab7b54a18f61589042161
Hanson Wong <hanson.wong01@gmail.com>
2025-02-19 16:20:10 -0500

    Audio plays after timer completes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
+import soundfile from './audio/valorantAssets/valorant-champions-2024-kill-1.mp3'
 import React, {useState} from 'react';
-
 
 
 function App() {
   let buttonCSS="py-2 px-3 m-1 rounded-md text-white border-2";
+  let pomodoroCSS = 'm-auto w-[40vw] items-center flex flex-col bg-red-400/80 py-10 rounded-md justify-center h-[60vh]';
+  let shortBreakCSS = 'm-auto w-[40vw] items-center flex flex-col bg-teal-400/80 py-10 rounded-md justify-center h-[60vh]';
+  let longBreakCSS= 'm-auto w-[40vw] items-center flex flex-col bg-cyan-400/80 py-10 rounded-md justify-center h-[60vh]';
+  let defaultCSS = 'm-auto w-[40vw] items-center flex flex-col bg-slate-500 py-10 rounded-md justify-center h-[60vh]'
   const [timer, setTimer] = useState("00:02");
   const [reset, setNewTimer] = useState("")
   const [SSButton, setSSButton] = useState(true)
+  const [timerBGColor, setTimerBGColor] = useState(defaultCSS)
   let startStopButton = (SSButton === true) ? "Start" : "Pause" 
+ 
+  function play(){
+    new Audio(soundfile).play()
+  }
 
   function startTimer (timer){
     let colon = timer.indexOf(":");
@@ -31,14 +40,12 @@ function App() {
     let displayTime = `${displayTimeMinutes}:${displayTimeSeconds}`
     setTimer(displayTime)
     if (countdown===0){
-      console.log("ran", countdown)
+      play()
       clearInterval(timerID)
-      setSSButton(SSButton =>!SSButton)
+      setSSButton(SSButton =>!SSButton) 
     }  
     countdown--
   }, 1000);}
-
-
 
   function setTimeValues(value){
     setNewTimer(value)
@@ -50,17 +57,23 @@ function App() {
     setTimer(reset)
   };
 
+  function changeTimeButton(time, CSS){
+    setTimeValues(time);
+    setTimerBGColor(CSS)
+  }
+
 
 
   return (
     <div className="bg-black w-screen h-screen flex">
-      <section className="m-auto w-[40vw] items-center flex flex-col bg-slate-500 py-10 rounded-md justify-center h-[60vh]">
+      <section className={timerBGColor}>
           <div>
-            <button onClick={()=>{setTimeValues("10:00")}} className={buttonCSS}>Pomodoro</button>
-            <button onClick={()=>{setTimeValues("5:00")}} className={buttonCSS}>Short Break</button>
-            <button onClick={()=>{(setTimeValues("03:00"))}}className={buttonCSS}>Long Break</button>
+            <button onClick={()=>{changeTimeButton("10:00", pomodoroCSS)}} className={buttonCSS}>Pomodoro</button>
+            <button onClick={()=>{changeTimeButton("05:00", shortBreakCSS)}} className={buttonCSS}>Short Break</button>
+            <button onClick={()=>{changeTimeButton("03:00", longBreakCSS)}}className={buttonCSS}>Long Break</button>
           </div>
           <h1 className="text-9xl font-bold text-white m-2">{timer}</h1>
+          <audio autoPlay src={soundfile}/>
           <div>
             <button onClick={()=>{setSSButton(SSButton =>!SSButton); startTimer(timer)}} className={buttonCSS}>{startStopButton}</button>
             <button onClick={()=>{resetButton()}}className={buttonCSS}>Reset</button>
